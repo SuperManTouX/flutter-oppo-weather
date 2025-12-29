6b5d69e2d6f551021baccc347d317b7266be8055
MT/馒头 <798521692@qq.com>
2025-12-29 20:23:24 +0800

    ✨ feat (detail - interface): 搭建细节界面框架 。

--- lib/pages/detail/hourly_tab_content.dart
+++ lib/pages/detail/hourly_tab_content.dart
@@ -1,10 +1,180 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_oppo_weather/models/display_city.dart';
+import 'package:flutter_oppo_weather/models/weather/hourly.dart';
+import 'package:flutter_oppo_weather/services/weather/qweather_service.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter_oppo_weather/widget/Icon.dart';
 
-class HourlyTabContent extends StatelessWidget {
-  const HourlyTabContent({super.key});
+class HourlyTabContent extends StatefulWidget {
+  // 接受DisplayCity对象
+  final DisplayCity location;
+  // 接受Hourly对象
+
+  const HourlyTabContent({super.key, required this.location});
+
+  @override
+  _HourlyTabContentState createState() => _HourlyTabContentState();
+}
+
+class _HourlyTabContentState extends State<HourlyTabContent> {
+  List<Hourly>? _hourlyData;
+
+  // 获取24小时天气数据
+  Future<void> _fetchWeather24h() async {
+    setState(() {});
+    try {
+      final service = QWeatherService();
+      final data = await service.getWeather24h(location: widget.location.id);
+      setState(() {
+        _hourlyData = data.hourly;
+      });
+    } catch (e) {
+      print('获取24小时天气数据失败: $e');
+      setState(() {});
+    }
+  }
+
+  @override
+  initState() {
+    super.initState();
+    _fetchWeather24h();
+  }
+
+  // 将小时温度数据转换为LineChart所需的FlSpot列表
+  List<FlSpot> _getTemperatureSpots() {
+    if (_hourlyData == null || _hourlyData!.isEmpty) {
+      return [];
+    }
+    final spots = <FlSpot>[];
+    for (int i = 0; i < _hourlyData!.length; i++) {
+      final temp = double.tryParse(_hourlyData![i].temp) ?? 0;
+      spots.add(FlSpot(i.toDouble(), temp));
+    }
+    return spots;
+  }
+
+  // 获取温度数据中的最小值
+  double _getMinTemp() {
+    if (_hourlyData == null || _hourlyData!.isEmpty) {
+      return 0;
+    }
+    double minTemp = double.infinity;
+    for (final data in _hourlyData!) {
+      final temp = double.tryParse(data.temp) ?? 0;
+      if (temp < minTemp) {
+        minTemp = temp;
+      }
+    }
+    return minTemp;
+  }
+
+  // 获取温度数据中的最大值
+  double _getMaxTemp() {
+    if (_hourlyData == null || _hourlyData!.isEmpty) {
+      return 30;
+    }
+    double maxTemp = -double.infinity;
+    for (final data in _hourlyData!) {
+      final temp = double.tryParse(data.temp) ?? 0;
+      if (temp > maxTemp) {
+        maxTemp = temp;
+      }
+    }
+    return maxTemp;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('首页内容'));
+    return Card(
+      color: Colors.blue,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.location.name,
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                Text('月日 今天明天', style: Theme.of(context).textTheme.titleLarge),
+              ],
+            ),
+            Text('6 分钟前发布', style: Theme.of(context).textTheme.titleLarge),
+            // 24小时天气折线趋势图
+            Container(
+              height: 200,
+              margin: const EdgeInsets.only(top: 16),
+              child: _hourlyData != null && _hourlyData!.isNotEmpty
+                  ? LineChart(
+                      LineChartData(
+                        gridData: FlGridData(show: false), // 隐藏网格
+                        titlesData: FlTitlesData(show: false), // 隐藏坐标轴文字
+                        borderData: FlBorderData(show: false), // 隐藏边框
+
+                        minX: 0,
+                        maxX: _hourlyData!.length - 1.toDouble(),
+                        minY: _getMinTemp() - 2,
+                        maxY: _getMaxTemp() + 2,
+                        lineBarsData: [
+                          LineChartBarData(
+                            spots: _getTemperatureSpots(),
+                            color: Colors.white,
+                            barWidth: 2,
+                            isStrokeCapRound: true,
+                            isCurved: false, // 直线（UI中是折线）
+                            dotData: FlDotData(show: false), // 隐藏折点
+                            belowBarData: BarAreaData(
+                              show: true,
+                              color: Colors.white.withOpacity(0.1),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : const Center(
+                      child: CircularProgressIndicator(color: Colors.white),
+                    ),
+            ),
+            // 24小时天气详细信息
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      '12:00',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Text('20℃', style: Theme.of(context).textTheme.titleLarge),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '12:00',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Text('20℃', style: Theme.of(context).textTheme.titleLarge),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      '12:00',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Text('20℃', style: Theme.of(context).textTheme.titleLarge),
+                  ],
+                ),
+                // 其他小时...
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/pages/detail/weather_detail.dart
+++ lib/pages/detail/weather_detail.dart
@@ -1,69 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_oppo_weather/models/weather/daily.dart';
+import 'package:flutter_oppo_weather/models/weather/hourly.dart';
+import 'package:flutter_oppo_weather/services/weather/qweather_service.dart';
 import '../../models/display_city.dart';
 import 'hourly_tab_content.dart';
 import 'daily_tab_content.dart';
 import 'future15_tab_content.dart';
 import 'air_quality_tab_content.dart';
 
-class WeatherDetail extends StatelessWidget {
+class WeatherDetail extends StatefulWidget {
   final DisplayCity location;
 
   const WeatherDetail({super.key, required this.location});
+
+  @override
+  State<WeatherDetail> createState() => _WeatherDetailState();
+}
+
+class _WeatherDetailState extends State<WeatherDetail>
+    with SingleTickerProviderStateMixin {
+  final List<String> _tabTitles = ['逐小时', '7天', '15天', '空气质量'];
+  bool _isLoading = false;
+  List<Daily>? _forecastData;
+  late TabController _tabController;
+
+  // 获取七天的天气数据
+  Future<void> _fetchWeatherForecast() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      final service = QWeatherService();
+      final data = await service.getWeather7d(location: widget.location.id);
+      setState(() {
+        _forecastData = data.daily;
+        _isLoading = false;
+      });
+    } catch (e) {
+      print('获取天气数据失败: $e');
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+ 
+
+  // 依据激活的tab，加载对应数据
+  Future<void> _loadDataForActiveTab(int index) async {
+    if (index == 0) {
+      // 逐小时天气数据
+
+    } else if (index == 1) {
+      await _fetchWeatherForecast();
+    } else if (index == 2) {
+      // TODO: 15天天气数据
+    } else if (index == 3) {
+      // TODO: 空气质量数据
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // 初始化 TabController
+    _tabController = TabController(length: 4, vsync: this);
+    // 添加 Tab 切换监听
+    _tabController.addListener(() {
+      print('当前tab索引: ${_tabController.index}');
+      if (!_tabController.indexIsChanging) {
+        // 当 Tab 切换完成时加载对应数据
+        _loadDataForActiveTab(_tabController.index);
+      }
+    });
+    // TODO：要从路由参数中获取进入的TabIndex
+    _loadDataForActiveTab(0); // 默认加载第一个tab的数据
+  }
+
+  @override
+  void dispose() {
+    // 销毁 TabController
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // DefaultTabController 管理 Tab 切换状态，length 是 Tab 数量
-    return DefaultTabController(
-      length: 4, // 必须和 Tab 数量一致
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('${location.name} 天气详情'),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          // 顶部 TabBar 配置
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(48),
-            child: SizedBox(
-              height: 48,
-              child: TabBar(
-                tabAlignment: TabAlignment.start, // 起始对齐 重要
-                isScrollable: true, // 开启滚动（适配固定宽度+多Tab场景）
-                indicatorSize: TabBarIndicatorSize.tab, // 指示器适配Tab宽度
-                indicatorColor: Colors.white, // 指示器颜色
-                indicatorWeight: 2, // 指示器高度（更细更协调）
-                // 文字颜色配置
-                unselectedLabelColor: const Color.fromARGB(179, 255, 255, 255),
-                labelColor: Colors.white,
-                // 文字样式（适配小高度）
-                labelStyle: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                ),
-                unselectedLabelStyle: const TextStyle(fontSize: 14),
-                // Tab 内边距（替代固定宽度，避免滚动偏移）
-                labelPadding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 2,
-                ),
-                // Tab 列表（天气场景适配）
-                tabs: const [
-                  Tab(text: '逐小时'),
-                  Tab(text: '每日'),
-                  Tab(text: '15日'),
-                  Tab(text: '空气质量'),
-                ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${widget.location.name} 天气详情'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // 顶部 TabBar 配置
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(48),
+          child: SizedBox(
+            height: 48,
+            child: TabBar(
+              controller: _tabController, // 使用我们自己的 TabController
+              tabAlignment: TabAlignment.start, // 起始对齐 重要
+              isScrollable: true, // 开启滚动（适配固定宽度+多Tab场景）
+              indicatorSize: TabBarIndicatorSize.tab, // 指示器适配Tab宽度
+              indicatorColor: Colors.white, // 指示器颜色
+              indicatorWeight: 2, // 指示器高度（更细更协调）
+              // 文字颜色配置
+              unselectedLabelColor: const Color.fromARGB(179, 255, 255, 255),
+              labelColor: Colors.white,
+              // 文字样式（适配小高度）
+              labelStyle: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
               ),
+              unselectedLabelStyle: const TextStyle(fontSize: 14),
+              // Tab 内边距（替代固定宽度，避免滚动偏移）
+              labelPadding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 2,
+              ),
+              // Tab 列表（天气场景适配）
+              tabs: _tabTitles.map((title) => Tab(text: title)).toList(),
             ),
           ),
         ),
-        // 2. Tab 对应的内容页
-        body: const TabBarView(
-          // 注意：TabBarView 的子组件顺序必须和 TabBar 一一对应
-          children: [
-            HourlyTabContent(),
-            DailyTabContent(),
-            Future15TabContent(),
-            AirQualityTabContent(),
-          ],
-        ),
+      ),
+      // 2. Tab 对应的内容页
+      body: TabBarView(
+        controller: _tabController, // 使用我们自己的 TabController
+        // 注意：TabBarView 的子组件顺序必须和 TabBar 一一对应
+        children: [
+          HourlyTabContent(
+            location: widget.location,
+          ),
+          DailyTabContent(),
+          Future15TabContent(),
+          AirQualityTabContent(),
+        ],
       ),
     );
   }
